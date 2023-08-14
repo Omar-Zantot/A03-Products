3a1371b0d7dec22f197b63d7253dd957c3effab3
Omar-Zantout <omar82199@gmail.com>
2023-08-14 13:56:12 +0300

    monday

--- src/app/UI-components/btn/btn.directive.ts
+++ src/app/UI-components/btn/btn.directive.ts
@@ -1,4 +1,12 @@
-import { Directive, HostBinding, Input } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  HostBinding,
+  HostListener,
+  Input,
+  OnInit,
+  Renderer2,
+} from '@angular/core';
 
 @Directive({
   // all directives and components selectors follow css selectors pattern
@@ -6,9 +14,28 @@ import { Directive, HostBinding, Input } from '@angular/core';
   // both syntax are valid to apply attributes or register events: host/@HostBinding-@HostListener
   host: {
     class: 'btn',
+    // style: 'background: red !important',
   },
 })
-export class BtnDirective {
+export class BtnDirective implements OnInit {
+  constructor(
+    private renderer: Renderer2,
+    private _el: ElementRef<HTMLElement>
+  ) {
+    console.log(this._el);
+    this.renderer.setStyle(this._el.nativeElement, 'background', 'green', 1);
+    // this._el.nativeElement.style.background = 'red';
+  }
+
+  ngOnInit(): void {}
   @Input() @HostBinding('class') color: 'btn-primary' | 'btn-secondary' =
     'btn-primary';
+
+  /**
+   * thia method repesent click me
+   * */
+  @HostListener('click', ['$event']) clickMe(event: Event) {
+    console.log(event);
+    console.log('I`m clicked');
+  }
 }
